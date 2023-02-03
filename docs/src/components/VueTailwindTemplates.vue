3a52f1dcccba9ef6e0f8978490457d608c6a800a
Demis Bellot <demis.bellot@gmail.com>
2023-02-03 20:02:45 +0800

    Update VueTailwindTemplates.vue

--- docs/src/components/VueTailwindTemplates.vue
+++ docs/src/components/VueTailwindTemplates.vue
@@ -103,6 +103,29 @@
       </div>
    </div>
 </section>   
+<section class="w-full flex flex-col justify-center text-center">
+   <div id="jamstack-tailwind" class="mt-4 mb-2">
+      <h3 class="text-gray-400 text-xl mb-2">Empty Vue Tailwind</h3>
+      <div class="flex flex-wrap justify-center">
+         <div>
+            <a class="archive-url hover:no-underline netcoretemplates_vue-vite" :href="zipUrl('NetCoreTemplates/web-tailwind')">
+               <div class="bg-white dark:bg-gray-800 px-4 py-4 mr-4 mb-4 rounded-lg shadow-lg text-center items-center justify-center hover:shadow-2xl dark:border-2 dark:border-pink-600 dark:hover:border-blue-600" style="min-width:150px">
+                  <div class="text-center font-extrabold flex items-center justify-center mb-2">
+                     <div class="text-4xl text-blue-400 my-3">
+                        <Icon icon="logos:html-5" class="w-14 h-14" />
+                     </div>
+                  </div>
+                  <div class="text-xl font-medium text-gray-700">Web</div>
+                  <div class="flex justify-center h-8"></div>
+                  <span class="archive-name px-4 pb-2 text-blue-600 dark:text-indigo-400">{{ projectZip }}</span>
+                  <div class="count mt-1 text-gray-400 text-sm"></div>
+               </div>
+            </a>
+            <a class="text-sm text-center mr-4" href="https://web-tailwind.web-templates.io">web-tailwind.web-templates.io</a>
+         </div>
+      </div>
+   </div>
+</section>   
 </div>
 </template>
 <script setup lang="ts">
