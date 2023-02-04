06f2928de3b82082b5839d4e861245e21d4a8e62
Demis Bellot <demis.bellot@gmail.com>
2023-02-04 17:20:52 +0800

    Update VueComponentGallery.vue

--- docs/src/components/VueComponentGallery.vue
+++ docs/src/components/VueComponentGallery.vue
@@ -22,7 +22,7 @@
                 <NavListItem title="DataGrid" href="/vue/gallery/datagrid" :iconSvg="Icons.DataGrid">
                     DataGrid Component Examples for rendering tabular data
                 </NavListItem>
-                <NavListItem title="AutoQuery Grid" href="/vue/gallery/autoquerygrid"
+                <NavListItem v-if="false" title="AutoQuery Grid" href="/vue/gallery/autoquerygrid"
                     :iconSvg="Icons.AutoQueryGrid">
                     Instant customizable UIs for calling AutoQuery CRUD APIs
                 </NavListItem>
